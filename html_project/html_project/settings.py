ff6bd30658e0bc6c53bb5ab2979ec48aa759c42c
PabloSanchez <sancheztorrespablo@gmail.com>
2024-08-14 12:03:15 +0200

    creamos app core & courses

--- html_project/html_project/settings.py
+++ html_project/html_project/settings.py
@@ -18,6 +18,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES_DIR = [BASE_DIR / 'html_project' / 'templates'] # new
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'html_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR # add this line to the end of TEMPLATES list
+            ], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
