99329ba64e92af9ffc88c9c11bde391b3c0f523d
PabloSanchez <sancheztorrespablo@gmail.com>
2024-08-15 17:32:58 +0200

    personalizamos el modelo creado

--- html_project/blog/admin.py
+++ html_project/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Damos de alta la entidad post en el admin
+admin.site.register(Post)
--- html_project/blog/models.py
+++ html_project/blog/models.py
@@ -3,7 +3,20 @@ from django.utils import timezone
 
 # Modelo para un post de un blog.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    author = models.CharField(max_length=100)
-    created_at = models.DateTimeField(default=timezone.now) # Cuando se ha creado esa entidad.
+    title = models.CharField(
+        verbose_name='Título',
+        max_length=200
+        )
+    content = models.TextField(
+        verbose_name='Contenido',
+        )
+    
+    author = models.CharField(
+        verbose_name='Autor',
+        max_length=100)
+    created_at = models.DateTimeField(
+        verbose_name='Fecha de creación',
+        default=timezone.now) # Cuando se ha creado esa entidad.
+    
+    def __str__(self) -> str:
+        return self.title
