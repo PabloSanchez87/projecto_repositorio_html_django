9aaad2d5731d0d0a7eea792ff00c284b38eb561e
PabloSanchez <sancheztorrespablo@gmail.com>
2024-08-15 17:25:11 +0200

    creamos un modelo para un post, dentro de la app blog/models

--- html_project/blog/models.py
+++ html_project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Modelo para un post de un blog.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now) # Cuando se ha creado esa entidad.
