dea3ea69678271d3fede85f8fa7f8e7821d24ed2
PabloSanchez <sancheztorrespablo@gmail.com>
2024-08-18 17:39:05 +0200

    creamos en el core.models una Class Contact para guardar los contactos del formulario

--- html_project/core/models.py
+++ html_project/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Contant(models.Model):
+    name = models.CharField(verbose_name='Nombre', max_length=200)
+    email = models.EmailField(verbose_name='Correo Electronico',
+                              max_length=255, 
+                              unique=True, 
+                              db_index=True, 
+                              blank=False, 
+                              null=False)
+    message = models.TextField(verbose_name='Mensaje', max_length=5000)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
