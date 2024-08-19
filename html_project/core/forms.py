c19f8a47f90e045cdc7cbbfdca81a9df7ae8b0a0
PabloSanchez <sancheztorrespablo@gmail.com>
2024-08-19 12:36:08 +0200

    Creamos el formulario para el registro de nuevos usarios. Validamos datos en la clase creada de UserRegustrationForm, creamos template, y la función en las views

--- html_project/core/forms.py
+++ html_project/core/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.models import User
 
+# Create your forms here.
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label="Nombre")
     email = forms.EmailField(label="Email")
@@ -12,9 +15,38 @@ class ContactForm(forms.Form):
         return message
     
     
-        
+# Formulario de inicio de sesión
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100, label="Nombre de usuario")
     password = forms.CharField(widget=forms.PasswordInput, label="Password")
-
     
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(max_length=100, label="Nombre de usuario")
+    first_name = forms.CharField(max_length=100, label="Nombre")
+    last_name = forms.CharField(max_length=100, label="Apellido")
+    email = forms.EmailField(label="Email")
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Repite Password")
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Las contraseñas no coinciden.")
+        
+        validate_password(password2)
+        return password2
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Este nombre de usuario ya está en uso.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este correo electrónico ya está registrado.")
+        return email
--- html_project/core/views.py
+++ html_project/core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from courses.models import Course
 from blog.models import Post
-from core.forms import ContactForm
+from core.forms import ContactForm, UserRegistrationForm
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
+
 
 
 # Importamos send_mail para enviar correos electrónicos
@@ -30,59 +33,58 @@ def about_us(request):
     return render(request, 'core/about_us.html')
 
 
-# # Vista para el login 
-# def login_view(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():  # Si el formulario es válido
-#             username = form.cleaned_data['username']  # Obtenemos el username del formulario
-#             password = form.cleaned_data['password']  # Obtenemos la contraseña del formulario
-            
-#             user = authenticate(username=username, password=password)  # Autenticamos al usuario
-#             if user is not None:  # Si el usuario existe
-#                 login(request, user)  # Logeamos al usuario
-                
-#                 # Obtener el valor del parámetro 'next' si existe
-#                 next_url = request.GET.get('next', None)
-                
-#                 # Si next_url está presente, redirigir allí, de lo contrario, redirigir a la página principal
-#                 if next_url:
-#                     return redirect(next_url)
-#                 else:
-#                     return redirect(reverse('core:home'))  # Redireccionamos a la página de inicio
-        
-#             else:
-#                 context = {
-#                     'form': form,  # Pasamos el formulario al contexto
-#                     'error': True,  # Indicamos que hay un error en el formulario
-#                     'error_message': 'Invalid username or password.',  # Mensaje de error si los datos no son válidos
-#                 }
-#                 return render(request, 'core/login.html', context)  # Renderizamos la página de login y pasamos el formulario al contexto
-#         else:
-#             context = {
-#                 'form': form,  # Pasamos el formulario al contexto
-#                 'error': True,  # Indicamos que hay un error en el formulario
-#                 'error_message': 'Invalid username or password.',  # Mensaje de error si los datos no son válidos
-#             }
-#             return render(request, 'core/login.html', context)  # Renderizamos la página de login y pasamos el formulario al contexto
-        
-#     else:
-#         form = LoginForm()  # Instanciamos el formulario vacío
-#         context = {
-#             'form': form,  # Pasamos el formulario al contexto
-#         }
-        
-#         return render(request, 'core/login.html', context)
 
 def logout_view(request):
     logout(request)  # Deslogeamos al usuario
     return redirect(reverse('core:home'))  # Redireccionamos a la página de login
 
 
-
 # Vista para el registro
 def register(request):
-    return render(request, 'core/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)  # Instanciamos el formulario con los datos enviados por POST
+        if form.is_valid():  # Validamos que los datos enviados por el formulario sean válidos
+            username = form.cleaned_data['username']  # Obtenemos el nombre de usuario del formulario
+            first_name = form.cleaned_data['first_name']  # Obtenemos el primer nombre del formulario
+            last_name = form.cleaned_data['last_name']  # Obtenemos el apellido del formulario
+            email = form.cleaned_data['email']  # Obtenemos el email del formulario
+            password1 = form.cleaned_data['password1']  # Obtenemos la contraseña del formulario
+
+            # Creamos un nuevo usuario con los datos obtenidos del formulario
+            user = User.objects.create_user(
+                                            username=username, 
+                                            email=email, 
+                                            password=password1)  
+            
+            # Actualizamos los campos de nombre y apellido
+            if user:
+                user.first_name = first_name
+                user.last_name = last_name
+                user.save()
+            
+            login(request, user)  # Logeamos al usuario     
+                
+            context = {
+                'form': form,  # Pasamos el formulario al contexto
+                'success_message': 'Usuario creado correctamente',  # Mensaje de éxito
+                'success': True,  # Indicamos que se ha creado correctamente el usuario
+            }    
+            return render(request, 'core/home.html', context)  # Renderizamos la página de registro con el contexto
+        
+        else:
+            context = {
+                'form': form,  # Pasamos el formulario al contexto
+                'error': True,  # Indicamos que hay un error en el formulario
+                'error_message': 'Invalid username or password.',  # Mensaje de error si los datos no son válidos
+            }
+            return render(request, 'core/register.html', context)  # Renderizamos la plantilla con errores
+    else:
+        form = UserRegistrationForm()  # Instanciamos el formulario vacío
+        context = {
+            'form': form,  # Pasamos el formulario al contexto
+            'error': False,  # Indicamos que no hay error en el formulario
+        }
+        return render(request, 'core/register.html', context)
 
 
 # Vista para el contacto
@@ -131,3 +133,46 @@ def contact(request):
     
     return render(request, 'core/contact.html', context)
 
+# # Vista para el login 
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():  # Si el formulario es válido
+#             username = form.cleaned_data['username']  # Obtenemos el username del formulario
+#             password = form.cleaned_data['password']  # Obtenemos la contraseña del formulario
+            
+#             user = authenticate(username=username, password=password)  # Autenticamos al usuario
+#             if user is not None:  # Si el usuario existe
+#                 login(request, user)  # Logeamos al usuario
+                
+#                 # Obtener el valor del parámetro 'next' si existe
+#                 next_url = request.GET.get('next', None)
+                
+#                 # Si next_url está presente, redirigir allí, de lo contrario, redirigir a la página principal
+#                 if next_url:
+#                     return redirect(next_url)
+#                 else:
+#                     return redirect(reverse('core:home'))  # Redireccionamos a la página de inicio
+        
+#             else:
+#                 context = {
+#                     'form': form,  # Pasamos el formulario al contexto
+#                     'error': True,  # Indicamos que hay un error en el formulario
+#                     'error_message': 'Invalid username or password.',  # Mensaje de error si los datos no son válidos
+#                 }
+#                 return render(request, 'core/login.html', context)  # Renderizamos la página de login y pasamos el formulario al contexto
+#         else:
+#             context = {
+#                 'form': form,  # Pasamos el formulario al contexto
+#                 'error': True,  # Indicamos que hay un error en el formulario
+#                 'error_message': 'Invalid username or password.',  # Mensaje de error si los datos no son válidos
+#             }
+#             return render(request, 'core/login.html', context)  # Renderizamos la página de login y pasamos el formulario al contexto
+        
+#     else:
+#         form = LoginForm()  # Instanciamos el formulario vacío
+#         context = {
+#             'form': form,  # Pasamos el formulario al contexto
+#         }
+        
+#         return render(request, 'core/login.html', context)
